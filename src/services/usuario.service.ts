858c274040db989192478676d5ee57f162c8e3d8
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-09-08 11:36:50 -0300

    feat: buscar usuario pelo ID

--- src/services/usuario.service.ts
+++ src/services/usuario.service.ts
@@ -1,15 +1,30 @@
 import prisma from "../prisma/client";
+import { IUsuario } from "../types/IUsuario.types";
 
-export async function buscar_usuarios(){
+export async function buscar_usuarios(): Promise<IUsuario[] | null> {
 
     try {
 
         const usuarios = await prisma.usuarios.findMany();
         return usuarios;
-        
+
     } catch (erro: any) {
-      
+
         console.error(erro);
         throw new Error(`Erro ao buscar Usuarios`);
+    };
+};
+
+export async function buscar_usuario_id(id: number): Promise<IUsuario | null> {
+
+    try {
+
+        const usuario = await prisma.usuarios.findUnique({ where: id });
+        return usuario;
+
+    } catch (erro: any) {
+
+        console.error(erro);
+        throw new Error(`Erro ao buscar usuario pelo ID`);
     };
 };
