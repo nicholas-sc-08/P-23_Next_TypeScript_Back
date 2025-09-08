01bbf2379a95851f7d466f935d015db276115bb4
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-09-08 12:35:31 -0300

    feat: rotas configuradas

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import router_usuario from "./routes/usuario.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(`/usuario`, router_usuario);
 
 export default app;
